dc85248b7d7d0b69d360fdd29e688329e924f81e
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-03-05 12:33:48 -0500

    sddf

--- vue-opendata/src/views/HomeView.vue
+++ vue-opendata/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue'
+const covidlist = ref('')
+async function covidData() {
+  let results = await fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json')
+  let data = await results.json()
+  covidlist.value = data.results
+}
+onMounted(() => {
+  covidData()
+})
 </script>
 
 <template>
