51e9cb801fdc4ae1efc14d5649813b90cded07aa
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-03-05 12:00:17 -0500

    asd

--- vue-opendata/src/router/index.js
+++ vue-opendata/src/router/index.js
@@ -10,8 +10,17 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/bronxdata',
+      name: 'Bronx',
+      path: '/brooklyndata',
+      name: 'Brooklyn',
+      path: '/manhattandata',
+      name: 'Manhattan',
+      path: '/queensdata',
+      name: 'Queens',
+      path: 'statenislanddata',
+      name: 'Staten Island',
+
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
