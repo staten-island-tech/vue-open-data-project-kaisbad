4ec1ea228b32b721e48383ecc667bfacea113ac0
kaisbad <158204041+kaisbad@users.noreply.github.com>
2025-03-10 12:28:20 -0400

    oops

--- vue-opendata/src/views/HomeView.vue
+++ vue-opendata/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-const covidlist = ref('')
+const covidlist = ref([])
 async function covidData() {
-  let results = await fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json')
-  let data = await results.json()
-  covidlist.value = data
+  try {
+    let results = await fetch('https://data.cityofnewyork.us/resource/rc75-m7u3.json')
+    let data = await results.json()
+    covidlist.value = data
+    console.log(data)
+  } catch (error) {
+    console.error('Error fetching COVID data:', error)
+  }
 }
+
 onMounted(() => {
   covidData()
 })
@@ -13,6 +19,12 @@ onMounted(() => {
 
 <template>
   <main>
-    <h1>{{ covidlist.case_count }}</h1>
+    <h1>COVID Case Counts</h1>
+    <div v-if="covidlist.length">
+      <p v-for="(cdata, index) in covidlist" :key="index">
+        {{ cdata.case_count }}
+      </p>
+    </div>
+    <p v-else>Loading data...</p>
   </main>
 </template>
